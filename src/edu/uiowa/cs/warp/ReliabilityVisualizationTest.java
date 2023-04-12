5dc04ab2fc3a1b936bb38fab588d3beac104dcbc
Annalisa Karacay <115434389+aekaracay@users.noreply.github.com>
2023-04-12 13:50:14 -0500

    Template for JUnit Tests

---
+++ src/edu/uiowa/cs/warp/ReliabilityVisualizationTest.java
@@ -0,0 +1,30 @@
+package edu.uiowa.cs.warp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ReliabilityVisualizationTest {
+
+	@Test
+	void displayVisualizationTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void createHeader() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void createData() {
+		fail("Not yet implemented");
+		//call to ReliabilityAnalysis
+	}
+	
+	@Test
+	void createTitle() {
+		fail("Not yet implemented");
+	}
+
+}
