d34ed70e138849758f23e706ffd328aea1900a96
Justin Lin <jlin60@uiowa.edu>
2023-04-21 23:02:24 -0500

    framework for ReliAnalTest class complete

---
+++ src/edu/uiowa/cs/warp/ReliabilityAnalysisTest.java
@@ -0,0 +1,85 @@
+package edu.uiowa.cs.warp;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import edu.uiowa.cs.warp.SystemAttributes.ScheduleChoices;
+
+public class ReliabilityAnalysisTest {
+	private static final double M =.9;
+	private static final double E2E = .99;
+	private static final int NUM_CHANNELS = 16;
+	
+	Program program1;
+	Program program2;
+	Program program3;
+	
+	WarpSystem warp1;
+	WarpSystem warp2;
+	WarpSystem warp3;
+	
+	@BeforeEach
+	void initializeTests() {
+		WorkLoad load1 = new WorkLoad(M, E2E, "Example.txt");
+		WorkLoad load2 = new WorkLoad(.65,.54, "Example1a.txt");
+		WorkLoad stressTest = new WorkLoad(.9,.9, "StressTest4.txt");
+		
+		warp1 = new WarpSystem(load1, NUM_CHANNELS, ScheduleChoices.PRIORITY);
+		warp2 = new WarpSystem(load2, 15, ScheduleChoices.PRIORITY);
+		warp3 = new WarpSystem(stressTest, 20, ScheduleChoices.PRIORITY);
+
+		program1 = warp1.toProgram();
+		program2 = warp2.toProgram();
+		program3 = warp3.toProgram();
+	}
+	
+	@Test
+	void verifyReliabilitiesTest() {
+		//method not yet implemented
+	}
+	
+	@Test
+	void getReliabilitiesTest() {
+		//method not yet implemented
+	}
+	
+	@Test
+	void buildReliabilitiesTest() {
+		//method not yet implemented
+	}
+	
+	@Test
+	void setHeaderRowTest() {
+		//method not yet implemented
+	}
+	
+	@Test
+	void getHeaderRowTest() {
+		//method not yet implemented
+	}
+	
+	@Test
+	void printRATableTest() {
+		//method not yet implemented
+	}
+	
+	@Test
+	void numTxPerLinkAndTotalTxCost() {
+		ReliabilityAnalysis reliAnalysis = new ReliabilityAnalysis(0);
+		WorkLoad load1 = new WorkLoad(M, E2E, "Example.txt");
+		FlowMap fMap = load1.getFlows();
+		ArrayList<Integer> actual = reliAnalysis.numTxPerLinkAndTotalTxCost(fMap.get("F0"));
+		ArrayList<Integer> expected = new ArrayList<Integer>();
+		expected.add(1);
+		expected.add(1);
+		expected.add(1);
+		expected.add(2);
+		for (int i=0;i<actual.size();i++) {
+			assertSame(expected.get(i),actual.get(i));
+		}
+	}
+	
+}
